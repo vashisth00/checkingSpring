2d1403a344023054657cfd419d68650293eabf10
Vashisth Bhushan <vb@gounravel.com>
2021-12-08 12:26:16 +0530

    first commit

---
+++ src/main/java/com/example/checkingSpring/CheckingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkingSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/checkingSpring/CheckingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkingSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
